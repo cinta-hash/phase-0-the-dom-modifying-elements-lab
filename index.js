2d2dcb679061aa7ba2ad7601f30dd9613d91d025
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-01-09 10:00:04 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove("main");
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.id = 'victory'
+newHeader.innerHTML = "jess is the champion"
+document.body.appendChild(newHeader);
+
+
+
